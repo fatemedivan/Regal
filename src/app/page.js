c3890838d3ce0b27f2d7bdfc6e4a511b0b493dee
fatemedivan <fatemedivan480@gmail.com>
2025-05-07 10:09:21 +0330

    fix : handle header container

--- src/app/page.js
+++ src/app/page.js
@@ -6,142 +6,144 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <header className="container mx-auto lg:ml-0 overflow-hidden pr-5 pt-8 mb-16 lg:flex justify-end items-center lg:gap-6 lg:pr-12 lg:pt-18 lg:mb-22 xl:mx-auto">
-        <div className="relative min-w-120 h-158 hidden pr-5 lg:block">
-          <Image
-            width={480}
-            height={632}
-            className="object-cover"
-            src="/img/header-desktop-1.png"
-            alt=""
-            priority
-            quality={100}
-          />
-        </div>
-        <div className="text-neutral-gray-13">
-          <div className="relative">
-            <h1 className="font-semibold leading-7.5 text-2xl md:font-bold md:text-5xl md:max-w-150 md:leading-14.5 lg:ml-52">
-              لباس هایی که{" "}
-              <span className="px-0.25 pb-1 pt-3 bg-cognac-tint-1 inline-block ">
-                داستان
-              </span>{" "}
-              شما را روایت می کنند
-            </h1>
-            <p className="mt-2 mb-3 text-sm leading-6 md:mb-4 md:leading-7 md:text-[1rem] lg:ml-52">
-              هر لباس با دقت و عشق طراحی شده تا به شما احساس زیبایی و اعتماد به
-              نفس بدهد.
-            </p>
-            <img
-              src="/img/shape-header.png"
-              className="hidden lg:block absolute -top-110 right-[55%] xl:-top-130"
-              alt=""
-            />
-          </div>
-          <div className="flex gap-2 items-center">
-            <div className="leading-4.5 text-xs text-neutral-gray-11 px-4 py-0.5 bg-neutral-gray-2 border-1 border-neutral-gray-4 rounded-100 md:py-2.5 md:px-6 md:text-sm md:leading-5 text-nowrap">
-              کالکشن‌های متنوع
-            </div>
-            <div className="leading-4.5 text-xs text-neutral-gray-11 px-4 py-0.5 bg-neutral-gray-2 border-1 border-neutral-gray-4 rounded-100 md:py-2.5 md:px-6 md:text-sm md:leading-5 text-nowrap">
-              مد و فشن
-            </div>
-            <div className="leading-4.5 text-xs text-neutral-gray-11 px-4 py-0.5 bg-neutral-gray-2 border-1 border-neutral-gray-4 rounded-100 md:py-2.5 md:px-6 md:text-sm md:leading-5 text-nowrap">
-              استایل‌های خاص
-            </div>
-            <div className="leading-4.5 text-xs text-neutral-gray-11 px-4 py-0.5 bg-neutral-gray-2 border-1 border-neutral-gray-4 rounded-100 md:py-2.5 md:px-6 flex gap-3 md:text-sm md:leading-5 text-nowrap">
-              رنگ‌بندی‌های جذاب
-              <div className="w-2 h-2 my-auto rounded-100 bg-[#D5AC97] md:w-4 md:h-4"></div>
-              <div className="w-2 h-2 my-auto rounded-100 bg-[#C5AF86] md:w-4 md:h-4"></div>
-              <div className="w-2 h-2 my-auto rounded-100 bg-[#977B71] md:w-4 md:h-4"></div>
-              <div>...</div>
-            </div>
-          </div>
-          <div className="flex items-center mt-6 mb-8 md:my-8">
-            <button className="ml-6.25 text-sm leading-5 rounded-lg bg-cognac-primery text-white flex justify-center items-center py-2.5 gap-2 px-15 md:py-3.25 md:px-6 md:text-[1rem] lg:leading-5.5 cursor-pointer">
-              مشاهده کالکشن‌ها
-              <Image
-                width={16}
-                height={16}
-                quality={100}
-                src="/img/arrow-left2.svg"
-                alt=""
-              />
-            </button>
+      <header className="overflow-hidden pr-5 pt-8 mb-16 lg:pr-12 lg:pt-18 lg:mb-22">
+        <div className="container mx-auto lg:ml-0 xl:mx-auto lg:flex justify-end items-center lg:gap-6">
+          <div className="relative min-w-120 h-158 hidden pr-5 lg:block">
             <Image
-              width={45}
-              height={80}
-              className="relative mb-7"
-              src="/img/Arrow.svg"
+              width={480}
+              height={632}
+              className="object-cover"
+              src="/img/header-desktop-1.png"
               alt=""
+              priority
               quality={100}
             />
           </div>
-          <div className="relative">
-            <Image
-              width={14}
-              height={14}
-              className="absolute w-14 h-14 -top-7 right-[57%] sm:right-74 lg:right-[55%] lg:w-26 lg:h-26 lg:-top-13"
-              src="img/header-logo.svg"
-              alt=""
-              quality={100}
-            />
-            <div className="flex gap-3 items-center overflow-x-hidden">
+          <div className="text-neutral-gray-13">
+            <div className="relative">
+              <h1 className="font-semibold leading-7.5 text-2xl md:font-bold md:text-5xl md:max-w-150 md:leading-14.5 lg:ml-52">
+                لباس هایی که{" "}
+                <span className="px-0.25 pb-1 pt-3 bg-cognac-tint-1 inline-block ">
+                  داستان
+                </span>{" "}
+                شما را روایت می کنند
+              </h1>
+              <p className="mt-2 mb-3 text-sm leading-6 md:mb-4 md:leading-7 md:text-[1rem] lg:ml-52">
+                هر لباس با دقت و عشق طراحی شده تا به شما احساس زیبایی و اعتماد
+                به نفس بدهد.
+              </p>
+              <img
+                src="/img/shape-header.png"
+                className="hidden lg:block absolute -top-110 right-[55%] xl:-top-130"
+                alt=""
+              />
+            </div>
+            <div className="flex gap-2 items-center">
+              <div className="leading-4.5 text-xs text-neutral-gray-11 px-4 py-0.5 bg-neutral-gray-2 border-1 border-neutral-gray-4 rounded-100 md:py-2.5 md:px-6 md:text-sm md:leading-5 text-nowrap">
+                کالکشن‌های متنوع
+              </div>
+              <div className="leading-4.5 text-xs text-neutral-gray-11 px-4 py-0.5 bg-neutral-gray-2 border-1 border-neutral-gray-4 rounded-100 md:py-2.5 md:px-6 md:text-sm md:leading-5 text-nowrap">
+                مد و فشن
+              </div>
+              <div className="leading-4.5 text-xs text-neutral-gray-11 px-4 py-0.5 bg-neutral-gray-2 border-1 border-neutral-gray-4 rounded-100 md:py-2.5 md:px-6 md:text-sm md:leading-5 text-nowrap">
+                استایل‌های خاص
+              </div>
+              <div className="leading-4.5 text-xs text-neutral-gray-11 px-4 py-0.5 bg-neutral-gray-2 border-1 border-neutral-gray-4 rounded-100 md:py-2.5 md:px-6 flex gap-3 md:text-sm md:leading-5 text-nowrap">
+                رنگ‌بندی‌های جذاب
+                <div className="w-2 h-2 my-auto rounded-100 bg-[#D5AC97] md:w-4 md:h-4"></div>
+                <div className="w-2 h-2 my-auto rounded-100 bg-[#C5AF86] md:w-4 md:h-4"></div>
+                <div className="w-2 h-2 my-auto rounded-100 bg-[#977B71] md:w-4 md:h-4"></div>
+                <div>...</div>
+              </div>
+            </div>
+            <div className="flex items-center mt-6 mb-8 md:my-8">
+              <button className="ml-6.25 text-sm leading-5 rounded-lg bg-cognac-primery text-white flex justify-center items-center py-2.5 gap-2 px-15 md:py-3.25 md:px-6 md:text-[1rem] lg:leading-5.5 cursor-pointer">
+                مشاهده کالکشن‌ها
+                <Image
+                  width={16}
+                  height={16}
+                  quality={100}
+                  src="/img/arrow-left2.svg"
+                  alt=""
+                />
+              </button>
               <Image
-                width={150}
-                height={150}
-                className="lg:hidden object-cover"
-                src="/img/header-mobile-1.png"
+                width={45}
+                height={80}
+                className="relative mb-7"
+                src="/img/Arrow.svg"
                 alt=""
                 quality={100}
               />
+            </div>
+            <div className="relative">
               <Image
-                width={150}
-                height={150}
-                className="lg:hidden object-cover"
-                src="/img/header-mobile-2.png"
+                width={14}
+                height={14}
+                className="absolute w-14 h-14 -top-7 right-[57%] sm:right-74 lg:right-[55%] lg:w-26 lg:h-26 lg:-top-13"
+                src="img/header-logo.svg"
                 alt=""
                 quality={100}
               />
+              <div className="flex gap-3 items-center overflow-x-hidden">
+                <Image
+                  width={150}
+                  height={150}
+                  className="lg:hidden object-cover"
+                  src="/img/header-mobile-1.png"
+                  alt=""
+                  quality={100}
+                />
+                <Image
+                  width={150}
+                  height={150}
+                  className="lg:hidden object-cover"
+                  src="/img/header-mobile-2.png"
+                  alt=""
+                  quality={100}
+                />
 
-              <Image
-                width={150}
-                height={150}
-                className="object-cover lg:hidden rounded-xl"
-                src="/img/header-mobile-3.png"
-                alt=""
-                quality={100}
-              />
-              <Image
-                width={254}
-                height={312}
-                className="hidden lg:block"
-                src="/img/header-desktop-2.png"
-                alt=""
-                quality={100}
-              />
-              <Image
-                width={254}
-                height={312}
-                className="hidden lg:block"
-                src="/img/header-desktop-3.png"
-                alt=""
-                quality={100}
-              />
-              <Image
-                width={254}
-                height={312}
-                className="hidden lg:block"
-                src="/img/header-desktop-4.png"
-                alt=""
-                quality={100}
-              />
-              <Image
-                width={54}
-                height={312}
-                className="hidden lg:block"
-                src="/img/header-desktop-5.png"
-                alt=""
-                quality={100}
-              />
+                <Image
+                  width={150}
+                  height={150}
+                  className="object-cover lg:hidden rounded-xl"
+                  src="/img/header-mobile-3.png"
+                  alt=""
+                  quality={100}
+                />
+                <Image
+                  width={254}
+                  height={312}
+                  className="hidden lg:block"
+                  src="/img/header-desktop-2.png"
+                  alt=""
+                  quality={100}
+                />
+                <Image
+                  width={254}
+                  height={312}
+                  className="hidden lg:block"
+                  src="/img/header-desktop-3.png"
+                  alt=""
+                  quality={100}
+                />
+                <Image
+                  width={254}
+                  height={312}
+                  className="hidden lg:block"
+                  src="/img/header-desktop-4.png"
+                  alt=""
+                  quality={100}
+                />
+                <Image
+                  width={54}
+                  height={312}
+                  className="hidden lg:block"
+                  src="/img/header-desktop-5.png"
+                  alt=""
+                  quality={100}
+                />
+              </div>
             </div>
           </div>
         </div>

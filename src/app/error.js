406b615eb1e58e40c9ac1a930d3a5ddd2bda62e6
fatemedivan <fatemedivan480@gmail.com>
2025-06-08 23:38:08 +0330

    change: modify error file

--- src/app/error.js
+++ src/app/error.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function error({error}) {
   return (
     <div className='flex justify-center items-center h-screen text-3xl'>
-      <h1>{error.message} please refresh or try again later</h1>
+      <h1 className='text-sm lg:text-lg'>{error.message} please refresh or try again later</h1>
     </div>
   )
 }
